1e805aaa269e9c2a86c081086e545ce65fb22a54
userhv <heliovictor05@hotmail.com>
2022-05-14 16:48:39 -0300

    update database

---
+++ backend/todo/database/crawler.py
@@ -0,0 +1,33 @@
+import tmdbsimple as tmdb
+import json
+
+tmdb.API_KEY = ''
+
+def convert(dic):
+  string_final = ""
+  for d in dic:
+      string_final += d['name'] + ","
+  string_final = string_final.rstrip(',')
+  return string_final
+
+def main():
+  content = {}
+  for i in range(100000,150000):
+    print(i)
+    try:
+      response = tmdb.Movies(i).info()
+      string_convert = convert(response['genres'])
+      if(response['release_date'] == ""):
+        response['release_date'] = None
+      
+      content = {"pk": i, "model": "todo.movies","fields": {"title": response['title'],"language": response['original_language'], "genres": string_convert, "overview": response['overview'],"popularity": response['popularity'], "poster_path": response['poster_path'], "release_date": response['release_date'],"vote_average": response['vote_average'],"vote_count":response['vote_count']}}
+      list_movies.append(content)
+    except:
+      continue
+
+
+list_movies = list()
+main()
+json_object = json.dumps(list_movies, indent= 4)
+with open("sample.json", "w") as outfile:
+  outfile.write(json_object)
--- backend/todo/database/db.py
+++ backend/todo/database/db.py
@@ -1,14 +1,13 @@
 from ..models import Movies
 
 
-b = Movies( title = "I Love You to Death", 
-    language = "en",
-    genres = "Action, Comedy, Crime",
-    overview= "Joey Boca is the owner of a pizza parlour, and has been married to Rosalie for years. When Rosalie discovers that Joey is a womanizer and has been cheating on her for a long time, she goes to extreme lengths to punish him.",
-    popularity = 6.977,
-    poster_path = "/8LdWJLUfEa5JeUJ7SljJQMIJikZ.jpg",
-    release_date = "1990-04-06",
-    vote_average = 6.4,
-    vote_count = 192)
+b = Movies( title = "", 
+    language = "",
+    genres = "",
+    overview = "",    
+    popularity = "",
+    poster_path = "",
+    release_date = "",
+    vote_average = "",
+    vote_count = "")
 
-b.save()
