2df676ccde35ed1795173a853ab5eb91b907e72d
Marcelo Mascarenhas <70808580+marcelo-mascarenhas@users.noreply.github.com>
2022-05-22 15:29:20 -0300

    Adding Recommendation Routes

--- backend/todo/api.py
+++ backend/todo/api.py
@@ -12,6 +12,7 @@ from unidecode import unidecode
 from .utils import getMovieDictionary
 from .crawlers.remove import remove_adult
 
+
 class Home(APIView):
   def get(self, request):
     # remove_adult()
@@ -20,9 +21,19 @@ class Home(APIView):
 
 class GetMostPopularMovies(APIView):
   """
-  Chamada da API responsável por retonar os X filmes mais populares.
+  Chamada da API responsável por retonar uma quantidade passada
+  de filmes mais populares.
   """
   def get(self, request, number):
+    """
+    
+    Parâmetros
+    ----------
+
+    (INT) number: Threshold que define quantos filmes 'mais populares' serão retornados
+    na chamada.
+    
+    """
     count_dict = {}
     o1 = Movies.objects.all().order_by('-popularity')[:number]
     final_response = getMovieDictionary(o1, title_as_key=False)
@@ -30,10 +41,26 @@ class GetMostPopularMovies(APIView):
     return Response(final_response)  
 
 class GetMovie(APIView):
+  """
+  Chamada da API que recuperam os filmes buscados.
+  """
+  
   authentication_classes = []
   permission_classes = []
   def get(self, request, movie_name, limit):
-    movies = {}
+    """
+    
+    Parâmetros
+    ----------
+
+    (STR) movie_name
+
+    (INT) limit : Delimitador que define até quantos filmes podem ser retornados, caso exista
+    mais de um filme com mesmo nome no database.
+    
+    """
+    
+    
     movie_name = unidecode(movie_name)
     o1 = Movies.objects.filter(title__contains=movie_name)[:limit]
     
@@ -43,9 +70,14 @@ class GetMovie(APIView):
 
 
 
-class ListUsers(APIView):
+class GetRecommendation(APIView):
   authentication_classes = []
   permission_classes = []
 
-  def get(self, request):
+  def get(self, request, movie_name, limit):
+    movie_name = unidecode(movie_name)    
+
+    # recommendation_dict = get_recommendations(movie_name, limit)
+    # return Response(recommendation_dict)
+    
     return Response({'ab': 3})
--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path(r"", api.Home.as_view(), name="Home"),
     path(r"api/movie/<movie_name>/<int:limit>", api.GetMovie.as_view(), name="api-movie"),
     path(r"api/popular_movies/<int:number>", api.GetMostPopularMovies.as_view(), name="api-most_pop_movies"),
-    path(r"api/data/a1", api.ListUsers.as_view(), name="api-data-ScoreDistribution"),
+    path(r"api/get_movie_rec/<movie_name>/<int:limit>", api.GetRecommendation.as_view(), name="api-recommendations"),
 ]
