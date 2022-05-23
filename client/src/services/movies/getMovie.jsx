e7f98cab531574b862a63061a71c4931158b852b
asafeclemente <asafecgm1@gmail.com>
2022-05-23 11:59:34 -0300

    recomendador

--- client/src/services/movies/getMovie.jsx
+++ client/src/services/movies/getMovie.jsx
@@ -13,7 +13,7 @@ export async function getMovieInfo(movie_info) {
 export async function getMovieRecommender(movie_info) {
     try {
       const response = await api.get(`/get_movie_rec/${movie_info}/12`);
-      return response.data;
+      return response.data.data;
     } catch (err) {
       throw new Error(err.response.data);
     }
