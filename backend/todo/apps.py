1568b11181498563402038f94ff2c2856fe7dcfe
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2022-11-02 16:31:19 -0300

    Removing comments

--- backend/todo/apps.py
+++ backend/todo/apps.py
@@ -11,7 +11,3 @@ class TodoConfig(AppConfig):
         path_list = (settings.PATH_TO_MODEL, settings.PATH_TO_ATT, settings.PATH_TO_MINS)
         #Initializing singleton;
         rec_obj_att = RecommenderAttributes(path_list)
-        
-        # load_topic_matrix(settings.PATH_TO_MODEL)
-        # load_parameters(settings.PATH_TO_ATT)
-        # load_mins(settings.PATH_TO_MINS)
