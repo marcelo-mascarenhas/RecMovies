32784d9e5f9d0842160896e2a74d01ec5be279c0
Henrique Magalhães <55369113+henriquemoc@users.noreply.github.com>
2023-05-19 02:11:43 -0300

    Adicionando Cliente no modelo

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -21,6 +21,7 @@ from django.db import models
 '''
 
 class Movies(models.Model):
+    
     title = models.CharField(max_length=120,blank=True, null=True)
     language = models.CharField(max_length=5,blank=True, null=True)
     genres = models.CharField(max_length=120,blank=True, null=True)
@@ -30,7 +31,29 @@ class Movies(models.Model):
     release_date = models.DateField(blank=True, null=True)
     vote_average = models.FloatField(blank=True, null=True)
     vote_count = models.IntegerField(blank=True, null=True)
+    available_copies = models.IntegerField(default=0)
+    price = models.FloatField(blank=True, null=True)
 
     def _str_(self):
         return self.title
 
+
+class Client(models.Model):
+    
+    client_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    instrument_purchase = models.CharField(max_length=100)
+    address_line = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=100)
+    money = models.FloatField(default=0.0)
+
+class MovieClient(models.Model):
+    
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movies, on_delete=models.CASCADE)
+
+
+
+    
